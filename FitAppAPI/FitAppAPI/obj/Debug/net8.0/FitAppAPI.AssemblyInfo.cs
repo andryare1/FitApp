f3e4a8d8b1d647e3a6b966bf798b8bc230ce6332
andryare1 <andreyyarema.2005@gmail.com>
2025-03-18 21:19:00 +0300

    измененный юрл для мака

--- FitAppAPI/FitAppAPI/obj/Debug/net8.0/FitAppAPI.AssemblyInfo.cs
+++ FitAppAPI/FitAppAPI/obj/Debug/net8.0/FitAppAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfb4b1bb4ba2da84f231adbd492321f65a1b8ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e5da7c363521ebb726c65e381337ab3d47092e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- urbanfit/lib/services/exercise_service.dart
+++ urbanfit/lib/services/exercise_service.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-//const baseUrl = 'http://192.168.31.166:5016/api'; // для макбука
-const baseUrl = 'http://192.168.31.142:5016/api';   // для ПК
+const baseUrl = 'http://192.168.31.85:5016/api'; // для макбука
+//const baseUrl = 'http://192.168.31.142:5016/api';   // для ПК
 
 class ExerciseService {
-  
-  Future<List<Map<String, dynamic>>> getExercisesByMuscleGroup(String muscleGroup) async {
-    final response = await http.get(Uri.parse('$baseUrl/exercises/group/$muscleGroup'));
+  Future<List<Map<String, dynamic>>> getExercisesByMuscleGroup(
+      String muscleGroup) async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/exercises/group/$muscleGroup'));
 
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
